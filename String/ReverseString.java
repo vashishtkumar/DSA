736bd05e36253512c6fa8a5f317e567f0525e6c1
vashishtkumar <vashishtk270@gmail.com>
2024-08-26 19:02:02 +0530

    ReverseString

---
+++ String/ReverseString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String str="vashisht";
+        StringBuilder str1=new StringBuilder(str);
+         System.out.println(str1.reverse());
+        
+    }
+}
