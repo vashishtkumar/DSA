4560fd9806ddf4022b8ba776bb79055501514848
vashishtkumar <vashishtk270@gmail.com>
2024-09-04 01:05:24 +0530

    2DArrayList

---
+++ ArrayList/TwoDArrayList.java
@@ -0,0 +1,22 @@
+package ArrayList;
+import java.util.*;
+public class TwoDArrayList {
+    public static void main(String[] args) {
+        ArrayList <ArrayList<Integer>> list=new ArrayList<>();
+
+        for(int i=0;i<3;i++){
+            ArrayList <Integer> li=new ArrayList<>();
+            for(int J=0;J<3;J++){
+            li.add(i);
+            }
+            list.add(li);
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+         System.err.print(list.get(i).get(j)+" ");
+            }
+System.out.println();
+        }
+    }
+}
